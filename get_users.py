1136fe9f30171e4c4db71ba1f7f32ba1a13df32b
luch-o <luiscarrodba97@gmail.com>
2024-09-04 15:40:33 -0500

    refactor function to work in the cloud and add permissions and packaging configuration

--- get_users.py
+++ get_users.py
@@ -3,16 +3,22 @@ import json
 import boto3
 from boto3.dynamodb.conditions import Key
 
+dynamodb_params = {}
+if os.getenv("IS_OFFLINE", False):
+    dynamodb_params = {
+        "region_name": "localhost",
+        "endpoint_url": "http://localhost:8000",
+        "aws_access_key_id": "default_access_key",
+        "aws_secret_access_key": "default_secret", 
+    }
 
-dynamodb = boto3.resource("dynamodb",
-                          region_name="localhost",
-                          endpoint_url="http://localhost:8000",
-                          aws_access_key_id="default_access_key",
-                          aws_secret_access_key="default_secret",  
-                          )
+dynamodb = boto3.resource("dynamodb", **dynamodb_params)                              
 table = dynamodb.Table("users-table")
 
-def handler(event, context):
-    response = table.query(KeyConditionExpression=Key("pk").eq("1"))
 
-    return {"statusCode": 200, "body": json.dumps(response)}
+def handler(event, context):
+    user_id = event["pathParameters"]["id"]
+    response = table.query(KeyConditionExpression=Key("pk").eq(user_id))
+    user = response["Items"][0] if response["Items"] else {}
+
+    return {"statusCode": 200, "body": json.dumps(user)}
