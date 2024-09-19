9d05fdc3aa8c63ed151ad6dd7ffba41a045a3e2c
luch-o <luiscarrodba97@gmail.com>
2024-09-18 22:52:17 -0500

    implement like user worker lambda function

--- like_user/like_user.py
+++ like_user/like_user.py
@@ -1,2 +1,25 @@
+import os
+import json
+import boto3
+from utils.utils import dynamodb_params
+
+dynamodb = boto3.resource("dynamodb", **dynamodb_params)                              
+table = dynamodb.Table(os.getenv("TABLE_NAME"))
+
 def handler(event, context):
-    print("evento recibido")
+    body = json.loads(event["Records"][0]["body"])
+    user_id = body["id"]
+    print(user_id)
+
+    response = table.update_item(
+        Key={
+            "pk": user_id
+        },
+        UpdateExpression="ADD likes :inc",
+        ExpressionAtributeValues={
+            ":inc": 1
+        },
+        ReturnValues="ALL_NEW",
+    )
+    
+    print(response)
