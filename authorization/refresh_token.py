7850d0f4773f5099afd0b5a177fe17aee2d66624
luch-o <luiscarrodba97@gmail.com>
2024-09-23 18:12:29 -0500

    fix bug, use id token instead of access token

--- authorization/refresh_token.py
+++ authorization/refresh_token.py
@@ -23,7 +23,7 @@ def handler(event, context):
         "body": json.dumps(
             {
                 key: response["AuthenticationResult"][key]
-                for key in ("AccessToken", "ExpiresIn")
+                for key in ("IdToken", "ExpiresIn")
             }
         )
     }
--- authorization/sign_in.py
+++ authorization/sign_in.py
@@ -25,7 +25,7 @@ def handler(event, context):
         "body": json.dumps(
             {
                 key: response["AuthenticationResult"][key]
-                for key in ("AccessToken", "RefreshToken", "ExpiresIn")
+                for key in ("IdToken", "RefreshToken", "ExpiresIn")
             }
         )
     }
